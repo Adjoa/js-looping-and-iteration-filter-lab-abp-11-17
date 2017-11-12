8dcda822d178a8d462e7332827a76716df2058eb
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-12 00:17:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+
+function filterItems(query) {
+  return fruits.filter(function(el) {
+      return el.toLowerCase().indexOf(query.toLowerCase()) > -1;
+  })
+}
+
+function findMatching(drivers, name) {
+    return drivers.filter(function(driver) { 
+      return driver.toLowerCase() === name.toLowerCase(); 
+    })
+}
+
+function fuzzyMatch(drivers, startLetters) {
+  return drivers.filter(function(driver) {
+    return driver.toLowerCase().indexOf(startLetters.toLowerCase()) > -1;
+  })
+}
