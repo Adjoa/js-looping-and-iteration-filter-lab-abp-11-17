feff187a7d77484c79db0e918fe71669966342b4
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-12 00:41:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
 // Code your solution in this file
-
-/*function filterItems(query) {
-  return fruits.filter(function(el) {
-      return el.toLowerCase().indexOf(query.toLowerCase()) > -1;
-  })
-}*/
-
 function findMatching(drivers, name) {
     return drivers.filter(function(driver) {
       return driver.toLowerCase() === name.toLowerCase();
     })
 }
 
-//drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
-
 function fuzzyMatch(drivers, startLetters) {
   return drivers.filter(function(driver) {
     return driver.toLowerCase().slice(0, startLetters.length) === startLetters.toLowerCase();
   })
 }
-
-//fuzzyMatch(drivers, 'Sa')
 
 function matchName(drivers, name) {
   return drivers.filter(function (driver) {
